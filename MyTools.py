d9e2df3169a415b4bd66eab262964e2d1fe9aaf8
EddyTheB <mail@eddybarratt.co.uk>
2016-09-29 16:45:24 +0100

    Changed MyTools.py to an actual file.

--- MyTools.py
+++ MyTools.py
@@ -1,1 +1,19 @@
-../../Python/FilesForPath/MyTools.py
+# MyTools
+# Eddy Barratt
+from os.path import expanduser
+
+def GetAPIKey(Name, Source='~/.api/api_keys'):
+    Source = expanduser(Source)
+    with open(Source, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if ';' in line:
+                line_ = line.split(';')
+                name = line_[0].strip()
+                key = line_[1].strip()
+                if name == Name:
+                    return key
+                    
+    print 'No key named {} exists.'.format(Name)                
+    return ''
+
